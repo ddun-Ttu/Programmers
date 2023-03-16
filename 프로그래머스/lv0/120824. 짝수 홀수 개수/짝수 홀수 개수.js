2891721e3e557328f394c8779d5d6ed11b571350
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-03-17 00:12:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,18 +1,16 @@
 function solution(num_list) {
     var answer = [];
-    var even = 0;
-    var odd = 0;
-    
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] % 2 == 0) {
-            even = even + 1;
-        } else {
-            odd = odd + 1;
-        }
+    var even = [];
+    var odd = [];
+
+    for ( var i = 0; i < num_list.length; i++ ) {
+       if ( num_list[i] % 2 == 0 ){
+           even.push(i)
+       } else {
+           odd.push(i)
+       }
     }
-    
-    answer[0] = even;
-    answer[1] = odd;
-    
+    answer.push(even.length, odd.length)
+
     return answer;
 }
