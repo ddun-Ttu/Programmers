5867a91848f831e43e5269090bc00ce80745f18d
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-03-22 23:42:54 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,12 +1,13 @@
 function solution(hp) {
     var answer = 0;
     
-    var a = parseInt(hp / 5);
-    var a1 = hp % 5;
-    var b = parseInt(a1 / 3 );
-    var c = a1 % 3;
+    var a = Math.floor(hp / 5); // 몫 ( 장군개미 몇 마리를 썼는지)
+    var a1 = hp % 5; // 나머지 (사냥감의 남은 체력)
+    var b = Math.floor( a1 / 3 ); // 나머지에 병정개미 몇마리 썼는지
+    var c = a1 % 3; // 나머지 남은 체력들
+    
     answer = a + b + c ;
     
-
+    
     return answer;
 }
