ef44f97dde63c644b302ea4f026551a36d777f90
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-03-23 13:04:25 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,18 +1,14 @@
 function solution(price) {
     var answer = 0;
-    var a = 0;
     
-    if (price >= 100000 && price < 300000) {
-        a = price* 0.95;
-    } else if ( price >= 300000 && price < 500000) {
-        a = price * 0.9; 
-    } else if ( price >= 500000){
-        a = price * 0.8;
+    if ( price >= 100000 && price < 300000) {
+        answer = price * 0.95;
+    } else if (price >= 300000 && price < 500000) {
+        answer = price * 0.9;
+    } else if (price >= 500000) {
+        answer = price * 0.8;
     } else {
-        a = price;
+        answer = price;
     }
-    
-    answer = Math.floor(a);
-    
-    return answer;
+    return Math.floor(answer);
 }
