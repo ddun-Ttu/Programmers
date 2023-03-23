804ca95a37023bd2c62805680133e8132bbc4c4c
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-03-23 13:14:12 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,10 +1,12 @@
 function solution(str1, str2) {
     var answer = 0;
-
-    var a = str1.includes(str2)
+    var str = str1.includes(str2);
     
-    var string = (a === true ? answer = 1 : answer = 2)
+        if ( str === true) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
     
-    console.log(a)
     return answer;
 }
