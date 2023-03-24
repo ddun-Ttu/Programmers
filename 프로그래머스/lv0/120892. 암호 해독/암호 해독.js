c61445a463c32fb8e5855446c287fcdf339c3417
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-03-24 16:05:41 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,12 @@
+function solution(cipher, code) {
+    var answer = '';
+    
+    for (var i = 1; i <= cipher.length; i++) {
+        if ( i % code === 0) {
+            answer += cipher[i - 1]
+        }
+    }
+    
+    
+    return answer;
+}
