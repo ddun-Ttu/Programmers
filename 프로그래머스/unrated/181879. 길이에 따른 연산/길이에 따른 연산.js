421919064890868a0623380b925e4e833269a0d5
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-05-21 21:47:46 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = 0;
+    
+   if (num_list.length >= 11) {
+        for (var i = 0; i < num_list.length; i++) {
+            answer += num_list[i];
+        }
+    } else if (num_list.length <= 10) {
+        answer = 1;
+        for (var i = 0; i < num_list.length; i++) {
+            answer *= num_list[i];
+        }
+    }
+    
+    return answer;
+}
