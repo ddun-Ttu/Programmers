6ed4380b0df00d348756e7676e94b0e440070bcb
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-12-08 20:58:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.43 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    var answer = [arr[0]];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1]) {
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
