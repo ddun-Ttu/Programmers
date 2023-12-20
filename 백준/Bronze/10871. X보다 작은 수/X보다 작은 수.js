de7929a007fac9be08b8532e5e9eb428706176e5
ddun-Ttu <117799204+ddun-Ttu@users.noreply.github.com>
2023-12-20 23:42:34 +0900

    [Bronze V] Title: X보다 작은 수, Time: 176 ms, Memory: 12536 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./text.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+
+const line1 = input[0].split(" ");
+const n = Number(line1[0]);
+const x = Number(line1[1]);
+
+let arr = input[1].split(" ");
+let result = "";
+for (i = 0; i < n; i++) {
+  if (arr[i] < x) {
+    result += arr[i] + " ";
+  }
+}
+console.log(result);
